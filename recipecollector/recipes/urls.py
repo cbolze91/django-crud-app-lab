d1c230106d552bb41666904a6448523d626ffeb2
Christina <christinamarieelaine@gmail.com>
2026-06-23 21:55:17 -0700

    Add recipe detail page

--- recipecollector/recipes/urls.py
+++ recipecollector/recipes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/', views.recipe_index, name='recipe-index'),
+    path('recipes/<int:recipe_id>/', views.recipe_detail, name='recipe-detail'),
 ]
--- recipecollector/recipes/views.py
+++ recipecollector/recipes/views.py
@@ -7,3 +7,11 @@ def home(request):
 def recipe_index(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/index.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    return render(
+        request,
+        'recipes/detail.html',
+        {'recipe': recipe}
+    )
